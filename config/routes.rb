7e975f54329c968a2809f84aec69d84989237093
nickschafran <nickschafran@gmail.com>
2015-10-01 15:00:22 -0400

    Add route for /contact

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
