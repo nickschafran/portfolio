98d21ca9a45f4703d69c1e30653974fee8eae9da
nickschafran <nickschafran@gmail.com>
2016-11-26 02:35:05 -0800

    Remove pages not in use from controller and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,4 @@
 class PagesController < ApplicationController
   def welcome
   end
-
-  def about
-  end
-
-  def contact
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
-
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
 end
