f1ec3440e2bad828e9ceb46c5f191b0127e3886a
nickschafran <nickschafran@gmail.com>
2016-11-25 20:39:03 -0800

    Serve static assets

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,5 +10,6 @@ Bundler.require(*Rails.groups)
 module Portfolio
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
